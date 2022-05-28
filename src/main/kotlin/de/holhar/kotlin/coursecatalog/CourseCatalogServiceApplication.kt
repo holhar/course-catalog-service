9317145b4a773b9701c8bf578ff3c9f6aaedf756
Holger Harms <holhar@posteo.de>
2022-05-28 21:56:21 +0200

    initial commit

---
+++ src/main/kotlin/de/holhar/kotlin/coursecatalog/CourseCatalogServiceApplication.kt
@@ -0,0 +1,11 @@
+package de.holhar.kotlin.coursecatalog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CourseCatalogServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CourseCatalogServiceApplication>(*args)
+}
---
+++ src/test/kotlin/de/holhar/kotlin/coursecatalog/CourseCatalogServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.holhar.kotlin.coursecatalog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseCatalogServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
