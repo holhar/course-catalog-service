06921ad906ef7609dc0be1ffaafa54b593e1d680
Holger Harms <holhar@posteo.de>
2022-05-30 11:59:12 +0200

    Add integration test for add new Course POST endpoint

---
+++ src/test/intg/de/holhar/kotlin/coursecatalog/controller/CourseControllerIT.kt
@@ -0,0 +1,38 @@
+package de.holhar.kotlin.coursecatalog.controller
+
+import de.holhar.kotlin.coursecatalog.dto.CourseDto
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class CourseControllerIT {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun addCourse() {
+
+        val courseDto = CourseDto(null, "Build Restful APIs using Kotlin and SpringBoot", "Development")
+
+        val savedCourseDto = webTestClient.post()
+            .uri("/v1/courses")
+            .bodyValue(courseDto)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDto::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue {
+            savedCourseDto!!.id != null
+        }
+    }
+}
