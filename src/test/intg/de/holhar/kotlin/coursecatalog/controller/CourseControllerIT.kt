858540c7bc147e9d2f475e5d98b54ce684e1cc6b
Holger Harms <holhar@posteo.de>
2022-05-30 13:26:50 +0200

    Add integration test for retrieve all courses GET endpoint

--- src/test/intg/de/holhar/kotlin/coursecatalog/controller/CourseControllerIT.kt
+++ src/test/intg/de/holhar/kotlin/coursecatalog/controller/CourseControllerIT.kt
@@ -1,7 +1,12 @@
 package de.holhar.kotlin.coursecatalog.controller
 
 import de.holhar.kotlin.coursecatalog.dto.CourseDto
+import de.holhar.kotlin.coursecatalog.entity.Course
+import de.holhar.kotlin.coursecatalog.repository.CourseRepository
+import de.holhar.kotlin.coursecatalog.util.courseEntityList
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
@@ -16,6 +21,16 @@ class CourseControllerIT {
 
     @Autowired
     lateinit var webTestClient: WebTestClient
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    internal fun setUp() {
+        courseRepository.deleteAll()
+        val courses = courseEntityList()
+        courseRepository.saveAll(courses)
+    }
 
     @Test
     fun addCourse() {
@@ -34,5 +49,20 @@ class CourseControllerIT {
         Assertions.assertTrue {
             savedCourseDto!!.id != null
         }
+    }
+
+    @Test
+    fun retrieveAllCourses() {
+
+        val courseDtos = webTestClient.get()
+            .uri("/v1/courses")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CourseDto::class.java)
+            .returnResult()
+            .responseBody
+
+        println("courseDts '$courseDtos'")
+        assertEquals(3, courseDtos!!.size)
     }
 }
