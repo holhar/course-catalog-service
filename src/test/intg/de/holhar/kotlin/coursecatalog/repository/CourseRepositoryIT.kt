83390122f2d7f9f45bb01fe0e0ba27288106f153
Holger Harms <holhar@posteo.de>
2022-05-30 15:01:10 +0200

    Testing multiple sets of data using @ParameterizedTest

--- src/test/intg/de/holhar/kotlin/coursecatalog/repository/CourseRepositoryIT.kt
+++ src/test/intg/de/holhar/kotlin/coursecatalog/repository/CourseRepositoryIT.kt
@@ -4,9 +4,13 @@ import de.holhar.kotlin.coursecatalog.util.courseEntityList
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -34,5 +38,24 @@ class CourseRepositoryIT {
         val courses = courseRepository.findByName("SpringBoot")
         println("courses '$courses'")
         Assertions.assertEquals(2, courses.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCoursesByName_parameterized(courseName: String, expectedSize: Int) {
+        val courses = courseRepository.findByName(courseName)
+        println("courses '$courses'")
+        Assertions.assertEquals(expectedSize, courses.size)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun courseAndSize() : Stream<Arguments> {
+            return Stream.of(
+                Arguments.arguments("SpringBoot", 2),
+                Arguments.arguments("Wiremock", 1)
+            )
+        }
     }
 }
